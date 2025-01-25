badbb3e40e0aca6397a2aa2e950aa17db2e45fb2
Aurelie Nogueira <aurelienog@gmail.com>
2025-01-25 21:47:27 +0100

    add navigate onClick

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,29 @@
 import React from 'react'
 import BeersImage from '../assets/beers.png'
+import newBeer from '../assets/new-beer.png'
+import random from '../assets/random-beer.png'
+import { useNavigate } from 'react-router-dom'
 
-export default function home() {
+export default function Home() {
+  const navigate = useNavigate();
+
   return (
     <div>
-      <section className='w-[40%] my-2 mx-6'>
+      <article className='w-[40%] my-2 mx-6' onClick={() => navigate('/beers')}>
         <img src={BeersImage} alt="" />
-        <h1 className='text-4xl my-2 mx-6'>All Beers</h1>
+        <h1 className='text-3xl my-2 mx-6'>All Beers</h1>
         <p className='text-gray-400 mx-6'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis nulla neque, assumenda id nihil labore voluptates? Rem, officiis laborum! Voluptates cupiditate labore perspiciatis, sit vitae eius molestias accusamus cum fugit?</p>
-      </section>
-      <section className='w-[40%] my-8 mx-6'>
-        <img src={BeersImage} alt="" />
-        <h1 className='text-4xl my-2 mx-6'>Random Beer</h1>
+      </article>
+      <article className='w-[40%] my-8 mx-6' onClick={() => navigate('/random-beer')}>
+        <img src={random} alt="" />
+        <h1 className='text-3xl my-2 mx-6'>Random Beer</h1>
         <p className='text-gray-400 mx-6'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis nulla neque, assumenda id nihil labore voluptates? Rem, officiis laborum! Voluptates cupiditate labore perspiciatis, sit vitae eius molestias accusamus cum fugit?</p>
-      </section>
-      <section className='w-[40%] my-8 mx-6'>
-        <img src={BeersImage} alt="" />
-        <h1 className='text-4xl my-2 mx-6'>New Beer</h1>
+      </article>
+      <article className='w-[40%] my-8 mx-6' onClick={() => navigate('/new-beer')}>
+        <img src={newBeer} alt="" />
+        <h1 className='text-3xl my-2 mx-6'>New Beer</h1>
         <p className='text-gray-400 mx-6'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis nulla neque, assumenda id nihil labore voluptates? Rem, officiis laborum! Voluptates cupiditate labore perspiciatis, sit vitae eius molestias accusamus cum fugit?</p>
-      </section>
+      </article>
     </div>
   )
 }
